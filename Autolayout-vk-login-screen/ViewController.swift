a78b4ad375d97fd76876ff9317d1d062f03bca81
iOS Warrior <ioswarrior@gmail.com>
2022-03-18 14:32:12 +0600

    Autolayout practice

--- Autolayout-vk-login-screen/ViewController.swift
+++ Autolayout-vk-login-screen/ViewController.swift
@@ -8,12 +8,134 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    let stackView = UIStackView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        setupScrollView()
+        setupLogo()
+        setupStackView()
     }
-
-
+    
+    let logoImage: UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo.png"))
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    let loginTextfield: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email and phone"
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.keyboardType = UIKeyboardType.emailAddress
+        textField.textColor = .black
+        textField.autocapitalizationType = .none
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 10
+        textField.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        textField.backgroundColor = .systemGray6
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        
+        return textField
+    }()
+    
+    let passwordTextfield: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.keyboardType = UIKeyboardType.default
+        textField.textColor = .black
+        textField.autocapitalizationType = .none
+        textField.layer.borderColor = UIColor.lightGray.cgColor
+        textField.layer.borderWidth = 0.5
+        textField.layer.cornerRadius = 10
+        textField.isSecureTextEntry = true
+        textField.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        textField.backgroundColor = .systemGray6
+        textField.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: textField.frame.height))
+        textField.leftViewMode = .always
+        
+        return textField
+    }()
+    
+    let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Log in", for: .normal)
+        button.setBackgroundImage(UIImage(named: "blue_pixel.png"), for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+        
+        return button
+    }()
+    
+    
+    func setupScrollView(){
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        NSLayoutConstraint.activate([
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
+        ])
+    }
+    
+    func setupLogo() {
+        contentView.addSubview(logoImage)
+        
+        NSLayoutConstraint.activate([
+            logoImage.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
+            logoImage.widthAnchor.constraint(equalToConstant:100),
+            logoImage.heightAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    func setupStackView() {
+        contentView.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fillEqually
+        
+        stackView.addArrangedSubview(loginTextfield)
+        stackView.addArrangedSubview(passwordTextfield)
+        stackView.addArrangedSubview(loginButton)
+        
+        stackView.setCustomSpacing(16, after: passwordTextfield)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            stackView.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 120),
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            
+            loginTextfield.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            loginTextfield.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            loginTextfield.heightAnchor.constraint(equalToConstant: 50),
+            passwordTextfield.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            passwordTextfield.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            passwordTextfield.heightAnchor.constraint(equalToConstant: 50),
+            
+            loginButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            loginButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16)
+        ])
+    }
 }
-
